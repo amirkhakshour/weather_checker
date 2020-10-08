b12c173cc67c31c1c38afc295789cf8c5be8bcbe
amir <amirkhakshour@gmail.com>
2020-10-08 20:04:24 +0200

    add python-dotenv and load .env file for base config

--- checker/config.py
+++ checker/config.py
@@ -1,6 +1,21 @@
-"""Default configuration"""
+"""Default configuration
 
+Use env var to override
+"""
+import os
+import distutils.util
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+READ_DOT_ENV_FILE = distutils.util.strtobool(os.getenv("READ_DOT_ENV_FILE", default="False"))
+if READ_DOT_ENV_FILE:
+    from dotenv import load_dotenv   # NOQA
+    load_dotenv()
+    env_path = BASE_DIR / '.env'
+    load_dotenv(dotenv_path=env_path)
 
+# Base config
+# ------------------------
+ENV = os.getenv("FLASK_ENV")
+DEBUG = ENV == "development"
+SECRET_KEY = os.getenv("SECRET_KEY")
