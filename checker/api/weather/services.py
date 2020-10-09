715754d3cd2969269f5e4f41b70c9d5c4d231be7
amir <amirkhakshour@gmail.com>
2020-10-09 10:19:39 +0200

    WeatherAPIRequester is responsible to request weather endpoint for a city

--- checker/api/weather/services.py
+++ checker/api/weather/services.py
@@ -1,4 +1,19 @@
 """Weather checker registry."""
+import requests
+from checker import config
+
+
+class WeatherAPIRequester:
+    @classmethod
+    def _build_req_url(cls, city, base_url=config.WEATHER_API_URL):
+        return '%s&%s' % (base_url, f'&q={city},de')
+
+    @classmethod
+    def get(cls, city):
+        # @todo add custom retry strategies on fail cases.
+        session = requests.Session()
+        result = session.request('get', cls._build_req_url(city))
+        return result.json(), result.status_code
 
 
 class WeatherCheckService:
