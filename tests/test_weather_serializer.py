7ba763a979766cd53c731ba5e30d7b7d1e5f5df8
amir <amirkhakshour@gmail.com>
2020-10-09 21:58:36 +0200

    WeatherSerializerTest to cover WeatherSerializer

---
+++ tests/test_weather_serializer.py
@@ -0,0 +1,28 @@
+import unittest
+
+
+class WeatherSerializerTest(unittest.TestCase):
+
+    def test_validate_returns_success_if_all_pass(self):
+        # @todo you get the idea ;)
+        pass
+
+    def test_invalid_data_populates_errors_attr(self):
+        # @todo you get the idea ;)
+        pass
+
+    def test_getting_data_without_calling_is_valid_fails(self):
+        # @todo you get the idea ;)
+        pass
+
+    def test_serializer_output_format(self):
+        # @todo you get the idea ;)
+        pass
+
+    def test_serializer_output_check_field_false_if_any_fails(self):
+        # @todo you get the idea ;)
+        pass
+
+    def test_serializer_output_criteria_field(self):
+        # @todo you get the idea ;)
+        pass
