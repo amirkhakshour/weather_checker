813b91720aff117114d158ff444abd5abc520490
amir <amirkhakshour@gmail.com>
2020-10-09 22:09:05 +0200

    CheckersTest to cover the registered checkers

---
+++ tests/test_checkers.py
@@ -0,0 +1,36 @@
+import unittest
+
+
+class CheckersTest(unittest.TestCase):
+
+    def test_check_name_len_is_odd_true_if_odd(self):
+        # @todo you get the idea ;)
+        pass
+
+    def test_check_name_len_is_odd_false_if_even(self):
+        # @todo you get the idea ;)
+        pass
+
+    def test_check_day_temp_is_normal_true_if_day_and_temp_in_range(self):
+        # @todo you get the idea ;)
+        pass
+
+    def test_check_day_temp_is_normal_true_if_night_and_temp_in_range(self):
+        # @todo you get the idea ;)
+        pass
+
+    def test_check_day_temp_is_normal_false_if_not_above_cases(self):
+        # @todo you get the idea ;)
+        pass
+
+    def test_check_rival_comparisons_check_fetches_rival_location(self):
+        # @todo you get the idea ;)
+        pass
+
+    def test_check_rival_comparisons_true_if_warmer(self):
+        # @todo you get the idea ;)
+        pass
+
+    def test_check_rival_comparisons_false_if_colder(self):
+        # @todo you get the idea ;)
+        pass
