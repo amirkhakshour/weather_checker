8e1cd70c09f1939ed6bda8c1da7bad5b7b544b50
amir <amirkhakshour@gmail.com>
2020-10-09 13:53:49 +0200

    ValidationError to validate serialization process

--- checker/api/weather/exceptions.py
+++ checker/api/weather/exceptions.py
@@ -2,6 +2,12 @@ class APIError(Exception):
     def __init__(
             self, message, http_body=None, http_status=None):
         super().__init__(message)
-        self.message = message
         self.http_body = http_body
         self.http_status = http_status
+        if not isinstance(message, dict) and not isinstance(message, list):
+            message = [message]
+        self.message = message
+
+
+class ValidationError(APIError):
+    pass
