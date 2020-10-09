7feef8e684c414ef9a8a2af74a7b301ce83115ec
amir <amirkhakshour@gmail.com>
2020-10-09 22:04:03 +0200

    WeatherCheckerResourceTest to cover WeatherChecker flask resource

---
+++ tests/test_weather_resources.py
@@ -0,0 +1,20 @@
+import unittest
+
+
+class WeatherCheckerResourceTest(unittest.TestCase):
+
+    def test_city_param_is_required(self):
+        # @todo you get the idea ;)
+        pass
+
+    def test_location_not_exits_returns_400_response(self):
+        # @todo you get the idea ;)
+        pass
+
+    def test_serialized_report_invalid_returns_500_response(self):
+        # @todo you get the idea ;)
+        pass
+
+    def test_get_verb_returns_serialized_report_for_queried_location(self):
+        # @todo you get the idea ;)
+        pass
